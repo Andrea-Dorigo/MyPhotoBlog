b0c7366d05b321856c491aff27ef3d01f8a3ca95
Andrea Dorigo <failosc@gmail.com>
2020-09-08 13:24:36 +0200

    added checkboxes and styled page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,15 +8,15 @@ class CommentsController < ApplicationController
   def create
     begin
       comment = Comment.new(comment_params)
-      # render plain: params[:comment].inspect
       comment.save!
-      cookies[:name] = comment.name;
-      cookies[:email] = comment.email;
+      if params[:remember_me]
+        cookies[:name] = comment.name;
+        cookies[:email] = comment.email;
+      end
     rescue
-      puts "rescued"
       #TODO: should pass with a parameter instead of hardcoding a string
       #TESTING
-      redirect_to home_url + "?error=invalidpost", data: { no_turbolink: true } and return
+      redirect_to home_url + "?error=invalidpost#add_comment_form", data: { no_turbolink: true } and return
     end
     redirect_to home_url, data: { no_turbolink: true } and return
   end
