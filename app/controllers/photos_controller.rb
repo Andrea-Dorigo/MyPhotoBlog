069123d3b61aee4206a47cbb4f453032edc1b8b6
Andrea Dorigo <failosc@gmail.com>
2020-10-05 17:12:40 +0200

    Replace session with cache and improve stability

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,36 +3,44 @@ class PhotosController < ApplicationController
   include HTTParty
 
   def index
-
+    session_key = cookies[:_myphotoblog_session]
     require 'open-uri'
     uri  = URI.parse(request.fullpath)
-    words_array = params['words'] ? params['words'].split("|") : []
+    @words_array = params['words'] ? params['words'].split("|") : []
+    @selected = params['selected'].to_i || 1
+    @photourl = []
     @comment = Comment.new
     @comments = Comment.all
+    @comment.name = cookies[:name]
+    @comment.email = cookies[:email]
 
-    puts "words array #{words_array}, selected = #{params[:selected].to_i}"
-    if words_array == []
-      photourl = []
+    puts "words array #{@words_array}, selected = #{params[:selected].to_i}"
+
+    if @words_array == []
       doc = HTTParty.get("https://www.randomlists.com/data/words.json")
       parsed = JSON.parse(doc.to_s)
       i = 0
       while i < 3
         word = parsed["data"].sample
-        puts "searching for #{word}"
+        puts "searching for: #{word}"
         photo = request_api("https://api.pexels.com/v1/search?query=#{word}&per_page=40")
         if photo["total_results"] >= 40
           k=0
           while k < 40
-            photourl.push(photo["photos"][k]["src"]["medium"])
+            @photourl.push(photo["photos"][k]["src"]["medium"])
             k += 1
           end
-          words_array.push(word)
+          @words_array.push(word)
           i += 1
         end
       end
-      session[:photourl] = photourl
-      session[:words_array] = words_array
-      redirect_to(home_url + "?selected=1&words=#{words_array[0]}|#{words_array[1]}|#{words_array[2]}")
+      Rails.cache.write("photourl_#{session_key}", @photourl, expires_in: 5.minutes)
+      redirect_to(home_url + "?selected=1&words=#{@words_array[0]}|#{@words_array[1]}|#{@words_array[2]}")
+    else
+      @photourl = get_photourl(@words_array)
+      Rails.cache.fetch("photourl_#{session_key}", expires_in: 5.minutes) do
+        @photourl
+      end
     end
   end
 
@@ -45,30 +53,54 @@ class PhotosController < ApplicationController
 
   def show_photos_js
     respond_to do |format|
-       format.js 
+       format.js {
+         session_key = cookies[:_myphotoblog_session]
+         @words_array = params['words'] ? params['words'].split("|") : []
+         @photourl = Rails.cache.fetch("photourl_#{session_key}", expires_in: 5.minutes) do
+           get_photourl(@words_array)
+         end
+         @selected = params['selected'].to_i || 1
+       }
        format.html {
-         words_array = session[:words_array]
-         redirect_to(home_url + "?selected=#{params[:selected]}&words=#{words_array[0]}|#{words_array[1]}|#{words_array[2]}")
+         @words_array = params['words'] ? params['words'].split("|") : []
+         redirect_to(home_url + "?selected=#{params[:selected]}&words=#{@words_array[0]}|#{@words_array[1]}|#{@words_array[2]}")
        }
     end
   end
 
   def create_comment
+    session_key = cookies[:_myphotoblog_session]
     @comment = Comment.new(params.require(:comment).permit(:name, :email, :body))
     uri  = URI.parse(request.fullpath)
     @words_array = params['words'] ? params['words'].split("|") : []
-    # ATTENZIONE: ORA QUANDO COMMENTI L'IMMAGINE SELEZIONATA TORNA QUELLA DI PARTENZA (fa il render di index questo metodo!)
     saved = @comment.save
     @comments = Comment.all
     @selected = params['selected'].to_i || 1
-    getPhotos
+
+    @photourl = Rails.cache.fetch("photourl_#{session_key}", expires_in: 5.minutes) do
+      get_photourl(@words_array)
+    end
 
     if saved
       puts "DEBUG>>> COMMENTO SALVATO"
-      session[:name] = @comment.name
-      session[:email] = @comment.email
+      cookies[:name] = @comment.name
+      cookies[:email] = @comment.email
       @comment = Comment.new
     end
     render 'index'
+  end
+
+
+  def get_photourl(words_array)
+    photourl = []
+    words_array.each do |word|
+      photo = request_api("https://api.pexels.com/v1/search?query=#{word}&per_page=40")
+      k=0
+      while k < 40
+        photourl.push(photo["photos"][k]["src"]["medium"])
+        k += 1
+      end
+    end
+    photourl
   end
 end
