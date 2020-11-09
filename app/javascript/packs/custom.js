9bbd20602b6943c65957802833a216644f96f19b
Andrea Dorigo <failosc@gmail.com>
2020-11-09 22:31:35 +0100

    Make navigation work with back and forward buttons

--- app/javascript/packs/custom.js
+++ app/javascript/packs/custom.js
@@ -1,3 +1,12 @@
 import Rails from "@rails/ujs";
 window.Rails = Rails;
-console.log("loaded custom.js");
+
+window.addEventListener('popstate', function(event) {
+    window.location.replace("/show_photo_gallery/" + window.location.search);
+}, false);
+
+window.onload = function(){
+    document.getElementById("refresh_icon").onclick = function(){
+        this.classList.add("rotating");
+    };
+};
