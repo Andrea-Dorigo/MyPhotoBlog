7f671131fb827a26c0e68d24d043020b248d841f
Andrea-Dorigo <failosc@gmail.com>
2021-03-08 16:53:23 +0100

    Prevent multiple refresh clicks

--- app/assets/javascripts/controllers/refresh_controller.js
+++ app/assets/javascripts/controllers/refresh_controller.js
@@ -1,10 +1,14 @@
 import { Controller } from "stimulus";
 
+var clickable = true;
+
 export default class extends Controller {
   rotate() {
-    document.getElementById("refresh_icon").classList.add("rotating");
+    this.element.classList.add("rotating");
+    clickable ? clickable = false : event.preventDefault() ;
   }
   connect() {
     document.getElementById("refresh_icon").classList.remove("rotating");
+    clickable = true;
   }
 }
