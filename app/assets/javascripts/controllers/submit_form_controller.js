fe3578165e298265e2d88f64718cf5dd45680adf
Andrea-Dorigo <failosc@gmail.com>
2021-03-15 18:36:48 +0100

    Remove scroll on submit It scrolls down even if there are validation errors. Disabled for now

--- app/assets/javascripts/controllers/submit_form_controller.js
+++ app/assets/javascripts/controllers/submit_form_controller.js
@@ -3,6 +3,6 @@ import { Controller } from "stimulus";
 export default class extends Controller {
   submit() {
     document.getElementById("comment_body").value = "";
-    window.scrollBy(0, 200);
+    // window.scrollBy(0, 200);
   }
 }
