7100bde2d22dfa77ae97e1bb7be509b54607c6fa
Andrea-Dorigo <failosc@gmail.com>
2021-03-16 19:36:20 +0100

    Render add_comment_form on submit

--- app/assets/javascripts/controllers/submit_form_controller.js
+++
@@ -1,8 +0,0 @@
-import { Controller } from "stimulus";
-
-export default class extends Controller {
-  submit() {
-    document.getElementById("comment_body").value = "";
-    // window.scrollBy(0, 200);
-  }
-}
