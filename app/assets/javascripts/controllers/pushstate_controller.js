f777ea99c757127bb4bdd7e3efc622b3cb7d6132
Andrea-Dorigo <failosc@gmail.com>
2021-03-18 18:45:27 +0100

    Rework pushstate (now set_attribute) stimulus controller

--- app/assets/javascripts/controllers/pushstate_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from "stimulus";
-
-
-export default class extends Controller {
-  push() {
-    document.getElementById("hidden_word").setAttribute("value", this.element.innerHTML);
-    history.pushState("data","title", this.element.href);
-  }
-}
